dd11104c21cbc727df11c7460cdc3cd40a26dc8d
harufumi.kamioka <kamioka.harufumi@gmail.com>
2019-10-02 16:24:52 +0900

    Initial Commit

---
+++ SCANIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCANIT
+//
+//  Created by Kamioka Harufumi on 2019/10/02.
+//  Copyright © 2019 Kamioka Harufumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
