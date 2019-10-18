f1dd21467b6e4a7687c07d29e21c5ebdc8bd29ac
harufumi.kamioka <kamioka.harufumi@gmail.com>
2019-10-18 14:27:51 +0900

    各種変更

--- SCANIT/CardListViewController.swift
+++ SCANIT/CardListViewController.swift
@@ -17,6 +17,14 @@ class CardListViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        performSegue(withIdentifier: "backSegue",sender: nil)
+    }
+    
+    @IBAction func soundBtn(_ sender: Any) {
+        
         let audioPath = Bundle.main.path(forResource: "halal", ofType:"mp3")!
         let audioUrl = URL(fileURLWithPath: audioPath)
         
@@ -38,13 +46,7 @@ class CardListViewController: UIViewController, AVAudioPlayerDelegate {
         audioPlayer.delegate = self
         audioPlayer.prepareToPlay()
         
-    }
-    
-    @IBAction func backBtn(_ sender: Any) {
-        performSegue(withIdentifier: "backSegue",sender: nil)
-    }
-    
-    @IBAction func soundBtn(_ sender: Any) {
+        
         if ( audioPlayer.isPlaying ){
             audioPlayer.stop()
         }
@@ -52,6 +54,140 @@ class CardListViewController: UIViewController, AVAudioPlayerDelegate {
             audioPlayer.play()
         }
     }
+    
+    @IBAction func soundVegetarianBtn(_ sender: Any) {
+        
+        let audioPath = Bundle.main.path(forResource: "vegetarian", ofType:"mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        } catch let error as NSError {
+            audioError = error
+            audioPlayer = nil
+        }
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        
+        
+        if ( audioPlayer.isPlaying ){
+            audioPlayer.stop()
+        }
+        else{
+            audioPlayer.play()
+        }
+    }
+    
+    
+    @IBAction func soundGlutenBtn(_ sender: Any) {
+        
+        
+        let audioPath = Bundle.main.path(forResource: "gluten_free", ofType:"mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        } catch let error as NSError {
+            audioError = error
+            audioPlayer = nil
+        }
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        
+        
+        if ( audioPlayer.isPlaying ){
+            audioPlayer.stop()
+        }
+        else{
+            audioPlayer.play()
+        }
+    }
+    
+    
+    @IBAction func soundCreditBtn(_ sender: Any) {
+        
+        
+        let audioPath = Bundle.main.path(forResource: "credit_card", ofType:"mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        } catch let error as NSError {
+            audioError = error
+            audioPlayer = nil
+        }
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        
+        
+        if ( audioPlayer.isPlaying ){
+            audioPlayer.stop()
+        }
+        else{
+            audioPlayer.play()
+        }
+    }
+    
+    
+    @IBAction func soundQrBtn(_ sender: Any) {
+        
+        let audioPath = Bundle.main.path(forResource: "qr_payment", ofType:"mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        } catch let error as NSError {
+            audioError = error
+            audioPlayer = nil
+        }
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        
+        
+        if ( audioPlayer.isPlaying ){
+            audioPlayer.stop()
+        }
+        else{
+            audioPlayer.play()
+        }
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
